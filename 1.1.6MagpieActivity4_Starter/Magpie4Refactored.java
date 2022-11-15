bf7e3e821fdabed2744c12806983fd8af3cfee42
jeiaso <79868802+jeiaso@users.noreply.github.com>
2022-11-15 01:02:51 -0800

    added filler counter to switch cases

--- 1.1.6MagpieActivity4_Starter/Magpie4Refactored.java
+++ 1.1.6MagpieActivity4_Starter/Magpie4Refactored.java
@@ -15,11 +15,13 @@ import java.util.Scanner;
 public class Magpie4Refactored{
     private Boolean greeting, introduction, weather, clarification, facts, filler, negative, respond, questioning;
     int currState;
+    int numFiller;
 
  
     public Magpie4Refactored(){
         greeting = true;
         currState = 0;
+        numFiller = 0; // keepts track of random conversation
     }
 	/**
 	 * Get a default greeting 	
@@ -49,18 +51,18 @@ public class Magpie4Refactored{
          switch(currState) {
            case 1:
              introduction=true;
-             System.out.println("I am the weather chatbot! What is your name?");
+             System.out.print("I am the weather chatbot! What is your name?");
              break;
 
             case 2:
              weather=true;
-             System.out.println("How was the weather today?");
+             System.out.print("How was the weather today?");
            // weatherConversation(statement);
              break;
 
             case 3:
              clarification=true;
-             System.out.println("I'm sorry, could you repeat that?");
+             System.out.print("I'm sorry, could you repeat that?");
              break;
 
            case 4:
@@ -79,61 +81,88 @@ public class Magpie4Refactored{
             else{
                 System.out.println(getRandomQuestion());
             }
-             break;
+            numFiller += 1;
+            if (numFiller > 5){
+                setState(14);
+            }
+            break;
 
             case 6:
             negative = true;
-            System.out.println("Why so negative?");
+            System.out.println("Why so negative? Here's a fun fact to cheer you up!");
+            setState(4);
+            break;
 
             case 7:
             respond = true; 
             System.out.println("Say something, please.");
+            setState(5);
+            break;
 
             case 8:
             questioning = true;
             System.out.print("I would rather learn about you than talk about me!"); 
+            break;
 
-            /* 
             // weather conversation
             case 9:
-            System.out.println("Nice! The weather the week of halloween was really cold; do you prefer hot or cold weather?");
+            System.out.print("Nice! The weather the week of halloween was really cold; do you prefer hot or cold weather?");
+            break;
 
             case 10:
-            System.out.println("Really? Me too! I'm glad it's finally winter. Why?");
+            System.out.print("Really? Me too! I'm glad it's finally winter. Why?");
+            endConversation();
+            break;
 
             case 11: 
-            System.out.println("Really? I prefer the cold! Why do you like hot weather?");
+            System.out.print("Really? I prefer the cold! Why do you like hot weather?");
+            endConversation();
+            break;
 
             case 12: 
-            System.out.println("Weather rhyme time! When it's cloudy I get pouty!");
+            System.out.print("Weather rhyme time! When it's cloudy I get pouty!");
+            break;
 
             case 13: 
-            System.out.println("A lot of people have different thoughts about the rain! Some think it's refreshing while others think it's sad.");
-            setState(5);
-            */
+            System.out.print("A lot of people have different thoughts about the rain! Some think it's refreshing while others think it's sad.");
+            endConversation();
+            break;
+
+            case 14:
+            System.out.print("It was nice getting to know you! I hope to talk to you more about weather later");
+            break;
           
       }
   
   }
+
+    public void endConversation(){
+        if (numFiller >5){
+            setState(14);
+        }
+        else{
+            setState(5);
+        }
+    }
 
     public void getFact(){
         final int randRandom = 5;
         double r = Math.random();
         int question = (int)(r * randRandom);
         if (question == 0){
-            System.out.println("Hmmm. That's interesting! Did you know two hurricanes can merge together and become one!?");
+            System.out.print("Hmmm. That's interesting! Did you know two hurricanes can merge together and become one!?");
         }
         else if (question == 1){
-            System.out.println("Rain is one of my favorites! Something I think is cool is that raindrops are shaped more like hamburger buns. As a rain drop falls, it becomes less spherical in shape and becomes more flattened on the bottom like a hamburger bun.");
+            System.out.print("Rain is one of my favorites! Something I think is cool is that raindrops are shaped more like hamburger buns. As a rain drop falls, it becomes less spherical in shape and becomes more flattened on the bottom like a hamburger bun.");
         }
         else if (question == 2){
-            System.out.println("Woah! That's fascinating! By the way, did you know it would take 1.3 million earths to fill up the sun!?");
+            System.out.print("Woah! That's fascinating! By the way, did you know it would take 1.3 million earths to fill up the sun!?");
         }
         else if (question == 3){
-            System.out.println("The cold? Interesting! Something I think that's interesing is it takes about an hour for snowflakes to fall to the ground!");
+            System.out.print("The cold? Interesting! Something I think that's interesing is it takes about an hour for snowflakes to fall to the ground!");
             }
         else{
-            System.out.println("Ooo tornadoes? That's very cool! In the whole world, the US gets the most tornadoes but what I find is interesting is that England is number three on that list!");
+            System.out.print("Ooo tornadoes? That's very cool! In the whole world, the US gets the most tornadoes but what I find is interesting is that England is number three on that list!");
         }
     }
 
@@ -180,7 +209,7 @@ public class Magpie4Refactored{
 		{
             setState(3);
 		}
-    /*else if (findKeyword(statement, "cold weather") >= 0)
+    else if (findKeyword(statement, "cold weather") >= 0)
     {
         setState(10);
     }
@@ -197,7 +226,7 @@ public class Magpie4Refactored{
     else if (findKeyword(statement, "rain") >= 0)
     {
         setState(13);
-    */
+    }
 
 		/*else if (findKeyword(statement, "mother") >= 0
 				|| findKeyword(statement, "father") >= 0
